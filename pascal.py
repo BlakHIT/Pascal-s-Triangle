d4282bb36d2a037bc2e6b05fccc54edca211331a
Aarush <aarushmagotra@protonmail.ch>
2020-12-09 20:30:36 +0530

    completed

---
+++ pascal.py
@@ -0,0 +1,95 @@
+def _copy(l, l2):
+    l2.clear()
+    for i in l:
+        l2.append(i)
+
+
+def _ls(l):
+    b = ''
+    for i in l:
+        c = str(i)
+        b += '{} '.format(c)
+    return b
+
+
+def main(n):
+    if n == 1:
+        print(1)
+
+    elif n == 2:
+        print(1)
+        print(1, 1)
+
+    elif n > 2:
+
+        lst = [1]
+        lst2 = []
+        a = 1
+
+        for i in range(n - 1):
+
+            if a == 1:
+                print(_ls(lst))
+            else:
+                print(1, _ls(lst))
+
+            if a == 1:
+                print(1, 1)
+                a += 1
+
+            _copy(lst, lst2)
+
+            lst3 = []
+
+            for j in range(len(lst2)):
+                elem = lst2[j - 1] + lst2[j]
+                lst3.append(elem)
+
+            lst3.append(1)
+            _copy(lst3, lst)
+
+
+print()
+print('Welcome to the "PASCAL\'s TRIANGLE"')
+
+while True:
+    try:
+
+        print()
+        choice = int(input('Enter the number of rows you want to print(0 -> quit): '))
+
+        if choice > 0:
+            print()
+            main(choice)
+            print()
+
+        elif choice == 0:
+            print()
+            print('Thanks for using :)')
+            print()
+            print('Bye!!!')
+            break
+
+        else:
+            print()
+            print('Invalid input :(')
+            print()
+            print('Value can\'t be Negative')
+            print()
+            print('Try Again')
+            print()
+
+        again = input('Would you like to print again(n -> no): ')
+        if again == 'n' or again == 'N':
+            print()
+            print('Thanks for using :)')
+            print()
+            print('Bye!!!')
+            break
+
+    except ValueError:
+        print()
+        print('Invalid input :(')
+        print()
+        print('Try Again')
+    
